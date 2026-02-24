598f66a3aed831f5d30d68fa72758aa56578d207
ASLeonard <alexander.leonard@usys.ethz.ch>
2026-02-24 08:37:43 +0100

    modernize build; bump min py version

--- upsetplot/__init__.py
+++ upsetplot/__init__.py
@@ -1,25 +1,27 @@
-__version__ = "0.10dev1"
+from importlib.metadata import PackageNotFoundError, version
 
-import os
+try:
+    __version__ = version("UpSetPlot")
+except PackageNotFoundError:
+    __version__ = "unknown"
 
-if os.environ.get("__IN-SETUP", None) != "1":
-    from .data import (
-        from_contents,
-        from_indicators,
-        from_memberships,
-        generate_counts,
-        generate_samples,
-    )
-    from .plotting import UpSet, plot
-    from .reformat import query
+from .data import (
+    from_contents,
+    from_indicators,
+    from_memberships,
+    generate_counts,
+    generate_samples,
+)
+from .plotting import UpSet, plot
+from .reformat import query
 
-    __all__ = [
-        "UpSet",
-        "generate_counts",
-        "generate_samples",
-        "plot",
-        "from_memberships",
-        "from_contents",
-        "from_indicators",
-        "query",
-    ]
+__all__ = [
+    "UpSet",
+    "generate_counts",
+    "generate_samples",
+    "plot",
+    "from_memberships",
+    "from_contents",
+    "from_indicators",
+    "query",
+]
